8e7bcd5a2b7b6c93ed25c37b7f48125089cfbd4c
Rubiya5 <rubiyauk@gmail.com>
2025-04-07 14:08:21 +0100

    Add files via upload

---
+++ 4_program.py
@@ -0,0 +1,17 @@
+myFruitList = ["apple", "banana", "Cherry"]
+print(myFruitList)
+myFruitList [2] = "orange"
+print(myFruitList)
+print(type(myFruitList))
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+myFavoriteFruitDictionary = {
+    "akua": "apple",
+    "sanvi": "banana",
+    "paulo": "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(myFavoriteFruitDictionary["paulo"])
+print(myFavoriteFruitDictionary["akua"])
+print(type(myFavoriteFruitDictionary))
